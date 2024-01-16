03574f6aebe60509200eb40e2889a8525952c87f
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2024-01-16 06:01:19 -0500

    update OpenAI model

--- src/pages/api/getPost.js
+++ src/pages/api/getPost.js
@@ -8,7 +8,7 @@ const handler = async (req) => {
   const prompt = `Write a ${mood} LinkedIn post based on the following prompt that exaggerates achievements, no matter how small. Fill with emotion, but maintain professional tone. Elaborate on inspiration for this achievement. In a separate paragraph, include a separate paragraph on how others can achieve this too. Be sure to include hashtags at the end. Use this prompt: ${input}`
 
   const payload = {
-    model: 'text-davinci-003',
+    model: 'gpt-3.5-turbo-instruct',
     prompt,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS ? parseInt(process.env.AI_MAX_TOKENS) : 200,
