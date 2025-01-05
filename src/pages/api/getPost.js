ba37e25b799c7fde977765533b23723c02e4131a
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2025-01-04 23:34:06 -0500

    increase limit

--- src/pages/api/getPost.js
+++ src/pages/api/getPost.js
@@ -5,13 +5,13 @@ export const config = { runtime: 'edge' }
 const handler = async (req) => {
   const { input, mood = 'typical' } = await req.json()
 
-  const prompt = `Write a ${mood} LinkedIn post based on the following prompt that exaggerates achievements, no matter how small. Fill with emotion, but maintain professional tone. Elaborate on inspiration for this achievement. In a separate paragraph, include a separate paragraph on how others can achieve this too. Be sure to include hashtags at the end. Use this prompt: ${input}`
+  const prompt = `Write a ${mood} LinkedIn post based on the following prompt that exaggerates achievements, no matter how small. Fill with emotion, but maintain professional tone. Elaborate on inspiration for this achievement. Include some inspiration for how others can achieve this too. Be sure to include hashtags at the end, and keep it no longer than 200 words. Use this prompt: ${input}`
 
   const payload = {
     model: process.env.OPENAI_MODEL_NAME,
     messages: [{ role: 'user', content: prompt }],
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
-    max_tokens: process.env.AI_MAX_TOKENS ? parseInt(process.env.AI_MAX_TOKENS) : 200,
+    max_tokens: process.env.AI_MAX_TOKENS ? parseInt(process.env.AI_MAX_TOKENS) : 400,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
@@ -36,6 +36,8 @@ const handler = async (req) => {
       text: `ERROR with API integration. ${data.error.message}`,
     })
   }
+
+  console.log(data)
 
   return NextResponse.json({ text: data.choices[0].message.content.split('\n') })
 }
