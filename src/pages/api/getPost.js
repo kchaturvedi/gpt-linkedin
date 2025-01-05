04be259a19524d01b199be7051e4bd2af2097387
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2025-01-04 23:26:20 -0500

    update api usage

--- src/pages/api/getPost.js
+++ src/pages/api/getPost.js
@@ -8,8 +8,8 @@ const handler = async (req) => {
   const prompt = `Write a ${mood} LinkedIn post based on the following prompt that exaggerates achievements, no matter how small. Fill with emotion, but maintain professional tone. Elaborate on inspiration for this achievement. In a separate paragraph, include a separate paragraph on how others can achieve this too. Be sure to include hashtags at the end. Use this prompt: ${input}`
 
   const payload = {
-    model: 'text-davinci-003',
-    prompt,
+    model: process.env.OPENAI_MODEL_NAME,
+    messages: [{ role: 'user', content: prompt }],
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS ? parseInt(process.env.AI_MAX_TOKENS) : 200,
     top_p: 1,
@@ -22,7 +22,7 @@ const handler = async (req) => {
     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
   }
 
-  const response = await fetch('https://api.openai.com/v1/completions', {
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
     headers: requestHeaders,
     method: 'POST',
     body: JSON.stringify(payload),
@@ -37,7 +37,7 @@ const handler = async (req) => {
     })
   }
 
-  return NextResponse.json({ text: data.choices[0].text.split('\n') })
+  return NextResponse.json({ text: data.choices[0].message.content.split('\n') })
 }
 
 export default handler
